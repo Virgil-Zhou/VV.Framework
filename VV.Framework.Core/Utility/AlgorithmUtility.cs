03dbc39f50063839e25af115870f43945b992a2e
VirgilZhou <virgilemail@163.com>
2017-05-19 12:02:39 +0800

    Add some methods

--- VV.Framework.Core/Utility/AlgorithmUtility.cs
+++ VV.Framework.Core/Utility/AlgorithmUtility.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,94 @@ namespace VV.Framework.Core
     /// </summary>
     public static class AlgorithmUtility
     {
+        /// <summary>
+        /// DES加密
+        /// </summary>
+        /// <param name="text">待加密文本</param>
+        /// <param name="key">8位秘钥</param>
+        /// <param name="encodingName">字符编码名称</param>
+        /// <returns>加密后的字符串</returns>
+        public static string DESEncryption(string text, string key, string encodingName = "UTF-8")
+        {
+            var encoding = Encoding.GetEncoding(encodingName);
+            var byteArray = encoding.GetBytes(text);
 
+            using (var desSvc = new DESCryptoServiceProvider())
+            {
+                desSvc.Mode = CipherMode.CBC;
+                desSvc.Padding = PaddingMode.PKCS7;
+                desSvc.Key = encoding.GetBytes(key);
+                desSvc.IV = encoding.GetBytes(key);
+
+                using (var ms = new MemoryStream())
+                {
+                    var cryStream = new CryptoStream(ms, desSvc.CreateEncryptor(), CryptoStreamMode.Write);
+                    cryStream.Write(byteArray, 0, byteArray.Length);
+                    cryStream.FlushFinalBlock();
+                    cryStream.Close();
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// DES解密
+        /// </summary>
+        /// <param name="text">待解密文本</param>
+        /// <param name="key">8位秘钥</param>
+        /// <param name="encodingName">字符编码名称</param>
+        /// <returns>解密后的字符串</returns>
+        public static string DESDecryption(string text, string key, string encodingName = "UTF-8")
+        {
+            var encoding = Encoding.GetEncoding(encodingName);
+            var byteArray = Convert.FromBase64String(text);
+
+            using (var desSvc = new DESCryptoServiceProvider())
+            {
+                desSvc.Mode = CipherMode.CBC;
+                desSvc.Padding = PaddingMode.PKCS7;
+                desSvc.Key = encoding.GetBytes(key);
+                desSvc.IV = encoding.GetBytes(key);
+
+                using (var ms = new MemoryStream())
+                {
+                    var cryStream = new CryptoStream(ms, desSvc.CreateDecryptor(), CryptoStreamMode.Write);
+                    cryStream.Write(byteArray, 0, byteArray.Length);
+                    cryStream.FlushFinalBlock();
+                    cryStream.Close();
+                    return encoding.GetString(ms.ToArray());
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="text">待加密文本</param>
+        /// <param name="capital">加密后的文本是否大写，true：大写 false：小写</param>
+        /// <param name="digit">加密字符串的位数，默认：32位，否则：16位</param>
+        /// <param name="encodingName">字符编码名称</param>
+        /// <returns>加密后的字符串</returns>
+        public static string MD5Encryption(string text, bool capital = true, int digit = 32, string encodingName = "UTF-8")
+        {
+            var encoding = Encoding.GetEncoding(encodingName);
+            using (var md5Svc = new MD5CryptoServiceProvider())
+            {
+                byte[] hashVals = md5Svc.ComputeHash(encoding.GetBytes(text));
+
+                var sb = new StringBuilder(32);
+                for (int i = 0; i < hashVals.Length; i++)
+                {
+                    if (capital)
+                        sb.Append(hashVals[i].ToString("X2"));
+                    else
+                        sb.Append(hashVals[i].ToString("x2"));
+                }
+
+                return digit == 32 ? sb.ToString() : sb.ToString().Substring(8, 16);
+            }
+        }
     }
 }
--- VV.Framework.Core/Utility/ImageUtility.cs
+++ VV.Framework.Core/Utility/ImageUtility.cs
@@ -1,16 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace VV.Framework.Core
 {
     /// <summary>
-    /// 
+    /// 提供图像操作常用成员
     /// </summary>
     public static class ImageUtility
     {
+        /// <summary>
+        /// 图像格式识别，path为空或null会引发ArgumentNullException异常
+        /// </summary>
+        /// <param name="path">文件的绝对路径</param>
+        /// <returns>返回图像格式枚举</returns>
+        public static ImageFormat FormatRecognition(string path)
+        {
+            if (path.IsNullOrWhiteSpace())
+                throw new ArgumentNullException(nameof(path), "图像路径不能为空");
 
+            var buffer = new Byte[4];
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                fs.Read(buffer, 0, buffer.Length);
+            }
+
+            var hexStr = buffer.FileHeaderHex();
+            return ImageFormatCollection.Get(hexStr);
+        }
+
+
+        /// <summary>
+        /// 图像格式识别，arr为null会引发ArgumentNullException异常
+        /// </summary>
+        /// <param name="arr">图像的字节数组</param>
+        /// <returns>返回图像格式枚举</returns>
+        public static ImageFormat FormatRecognition(byte[] arr)
+        {
+            if (arr == null)
+                throw new ArgumentNullException(nameof(arr), "图像字节数组不能为空");
+
+            var hexStr = arr.FileHeaderHex();
+            return ImageFormatCollection.Get(hexStr);
+        }
+
+
+        /// <summary>
+        /// 图像格式识别，postedFile为null会引发ArgumentNullException异常
+        /// </summary>
+        /// <param name="postedFile">上载文件对象</param>
+        /// <returns>返回图像格式枚举</returns>
+        public static ImageFormat FormatRecognition(HttpPostedFileBase postedFile)
+        {
+            if (postedFile == null)
+                throw new ArgumentNullException(nameof(postedFile), "上载文件对象不能为空");
+
+            byte[] buffer;
+            using (var br = new BinaryReader(postedFile.InputStream))
+            {
+                buffer = br.ReadBytes(4);
+            }
+
+            var hexStr = buffer.FileHeaderHex();
+            return ImageFormatCollection.Get(hexStr);
+        }
     }
 }
